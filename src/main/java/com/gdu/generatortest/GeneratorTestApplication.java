e851dd4477bd3deda29fe019c4b9a6b47c618fa7
Jiheui <developer.jiheuilee@gmail.com>
2024-03-13 20:17:00 +0900

    server connected

---
+++ src/main/java/com/gdu/generatortest/GeneratorTestApplication.java
@@ -0,0 +1,13 @@
+package com.gdu.generatortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdu/generatortest/GeneratorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdu.generatortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
